666ec8bb2d4c899868f35302fa297301f288e9da
Eugene Fuller <32392316+Cryptogenysis@users.noreply.github.com>
2022-03-08 04:30:50 -0800

    Update NewsFeed.js

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,35 +1,47 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const NewsFeed = () => {
+function NewsFeed() {
     const [articles, setArticles] = useState(null)
 
     useEffect(() => {
-
         const options = {
             method: 'GET',
-            url: 'http://localhost:8000/news'
-        }
-
-        axios.request(options).then((response) => {
+            url: 'https://crypto-news-feed1.p.rapidapi.com/feed',
+            params: {
+              sources: 'nytimes,newsbtc,economictimes,businessinsider,cointelegraph,coinrivet,coindesk',
+              exclude: '',
+              query: 'bitcoin,ethereum,nft,mana,decentraland,dogecoin,binance,splinterlands,vitalik,Qtum',
+              limit: '100'
+            },
+            headers: {
+              'x-rapidapi-host': 'crypto-news-feed1.p.rapidapi.com',
+              'x-rapidapi-key': 'b303be1ab3msh3866fa5749bb4dap18f264jsnb4ca3d637234'
+            }
+          };
+        axios.request(options).then(function (response) {
             console.log(response.data)
             setArticles(response.data)
 
-        }).catch((error) => {
+        }).catch(function (error) {
             console.error(error)
         })
     }, [])
 
     console.log(articles)
-
-    const first7Articles = articles?.slice(0,7)
+    const first7Articles = articles?.slice(0, 100)
 
     return (
-        <div className="news-feed">
-            <h2>News Feed</h2>
+        <div className="news-feed" >
+            <h2>🕖📈<u>Trending News</u>🗣📢</h2>
             {first7Articles?.map((article, _index) => (
                 <div key={_index}>
-                    <a href={article.url}><p>{article.title}</p></a>
+
+                    <p>
+                        <a href={article.url} target="_blank"><b>{article.title}</b></a><br />
+                        <i>{article.source}</i>
+                    </p>
+
                 </div>))}
         </div>
     )
